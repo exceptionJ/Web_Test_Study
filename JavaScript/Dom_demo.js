1f9440b5e51275276eb7cf2e53f4ab1a808c95b5
exceptionJ <27816803+exceptionJ@users.noreply.github.com>
2019-06-27 21:31:51 +0800

    dom_demo

--- JavaScript/Dom_demo.js
+++ JavaScript/Dom_demo.js
@@ -1,7 +1,5 @@
-
-
 //     var box = document.getElementById('box');
-    // window.onload = function(){
+// window.onload = function(){
 //     //alert(box.tagName);//获取元素节点标签名
 //     //alert(box.innerHTML);//获取元素节点标签下的文本
 //     document.write(box.id +'\r\n');
@@ -10,7 +8,25 @@
 //     document.write(box.title+'\r\n');            //获取元素属性值
 // };
 
-window.onload =function(){
-    var i = document.getElementsByTagName('box');
-    document.write(i.length[0]);
-}
+// window.onload = function () {
+//     var i = document.getElementsByTagName('li');
+//     //document.write(i.length);
+//     //document.write(i[0]);
+//     // document.write(i[0].tagName);
+//     //document.write(i[2].innerHTML);
+//     var body = document.getElementsByTagName('body')[0];
+//     document.write(body.tagName);
+// }
+// window.onload =function(){
+//     var i= document.getElementById('box');
+
+//     //var  list= i.childNodes.length;
+//     //var list =i.childNodes[0].nodeName;
+//     //var list =i.childNodes[0].nodeType;
+//     var list =i.childNodes[0].nodeValue;
+
+ 
+//      document.write(list);
+
+
+// }
