22a43d74a2f223fc0f6c572243d35773026acf92
exceptionJ <27816803+exceptionJ@users.noreply.github.com>
2019-07-06 21:48:49 +0800

    enter

---
+++ JavaScript/DOm.js
@@ -0,0 +1,43 @@
+// window.onload = function () {
+//     var doc = document.getElementById('list')
+//     alert(doc.id);
+//     alert(doc.style.color);
+//     alert(doc.className);
+//     alert(doc.title);
+// }
+
+// window.onload = function () {
+//     var doc = document.getElementById('list')
+//     alert(doc.tagName);//获取标签名
+//     doc.innerHTML='加入的内容';
+//     alert(doc.innerHTML);//获取这个标签里面的文本
+// }
+
+// window.onload =function () {
+//     var tg=document.getElementsByTagName('*');
+
+//    // alert(tg.length);
+//     for(var i=0;i<=tg.length;i++){
+//             tg[i]=i
+//             alert(tg[i].tagName);
+//     }
+
+// }                
+
+
+// window.onload = function () {
+//     var box = document.getElementById('t01');
+//      var zjd =box.childNodes;
+//      // alert(zjd.length);
+//     for(var i=0;i<=zjd.length;i++){
+//     // alert(zjd[i]);
+//       }
+//     alert(zjd[0]);
+//    alert(zjd[0].nodeType);
+//    alert(zjd[0].nodeValue);
+// }
+
+
+window.onload =function () {
+    
+}
