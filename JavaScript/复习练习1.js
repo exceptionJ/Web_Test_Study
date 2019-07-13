8bb00360d84ff39463905c3009f2c8b4f0ac50c6
exceptionJ <27816803+exceptionJ@users.noreply.github.com>
2019-07-13 18:30:11 +0800

    Update 复习练习1.js

--- JavaScript/复习练习1.js
+++ JavaScript/复习练习1.js
@@ -75,16 +75,43 @@ window.onload = function () {
     // }
 
     //const con =5;
+
+    //     function sum (x,y){
+    //         return x*y
+
+    //     }
+    //     function sum2(a,b){
+    //         return a+=b
+    //     }
+
+    //     var all=sum2.call(sum,2,5);
+
+    //     alert(all);
+
+
+    function outerFunction() {
+        var counter = 0;
+
+        function innerFunction() {
+            return counter += 1;
+        }
+        return innerFunction;
+        /*
+         注意 typeof innerFunction 是：function；而typeof innerFunction（）是number；
+        */
+    }
+    var add = outerFunction();
+
+    /* 
+    调用 outerFunction()返回的是内部函数innerFucntion,那么调用几次add()将调用几次
+    内部函数inner Function，内部函数公用了counter，所以能够计数,所以说闭包就是将内部嵌套函数变成外部可调用的。
+    */
+
+    alert(add());
+    alert(add());alert(add());alert(add());alert(add());
     
-    function sum (x,y){
-        return x*y
-       
-    }
-    function sum2(a,b){
-        return a+=b
-    }
 
-    var all=sum2.call(sum,2,5);
 
-    alert(all);
+
+
 }
